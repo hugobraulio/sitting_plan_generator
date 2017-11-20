b8129edffb5256d2241d5ad665480c0d84291a8a
Hugo Vazquez Alonso <hugobraulio@gmail.com>
2017-11-20 12:26:25 +0100

    Added a localhost URL to work with. In this case, my machine (MacbookAir) works on port 3000 of localhost.

--- background.js
+++ background.js
@@ -8,7 +8,7 @@
 var url_DDBS_course=""; //content.js will use it for ajax-quering the Dhammic DataBase Server (normally CALM - 06/2017) 
 var regex_CALM_production = /https:\/\/calm\.dhamma\.org\/(\w|-)+\/courses\/\d+\/course_applications*/; //All CALM4 languages
 var regex_CALM_test = /https:\/\/test\.calm\.dhamma\.org\/(\w|-)+\/courses\/\d+\/course_applications*/; //SPGv2
-//add other DDBS
+var regex_CALM_localhost = /http:\/\/localhost:3000\/(\w|-)+\/courses\/\d+\/course_applications*/;
 //var regex_VRAY_production = "........."; 
 //var regex_VRAY_test = ".........."; 
 
@@ -23,26 +23,26 @@ var calm_tabId = 0;
 function disableBrowserAction(){
     chrome.browserAction.setIcon({path:"./icons/disabled.png"});
     chrome.browserAction.setTitle({title:"Select a CALM4 webpage to ACTIVATE"});
-	chrome.browserAction.setPopup({popup: "popup_disabled.html"});
+	chrome.browserAction.setPopup({popup: "html/popup_disabled.html"});
     //chrome.tabs.executeScript(null, {file: "togglecontentscript.js"})
 }
 
 function enableBrowserAction(){
     chrome.browserAction.setIcon({path:"./icons/enabled.png"});
     chrome.browserAction.setTitle({title:"Seating Plan Generator"});
-	chrome.browserAction.setPopup({popup: "popup_enabled.html"});
+	chrome.browserAction.setPopup({popup: "html/popup_enabled.html"});
     //chrome.tabs.executeScript(null, {file: "contentscript.js"});
 }
 
 function updateState(tab){
 	var url = tab.url;
-	var enabled =  regex_CALM_test.test(url) || regex_CALM_production.test(url); //regExCALM_desarrollo.test(url) ||
+	var enabled =  regex_CALM_test.test(url) || regex_CALM_production.test(url) || regex_CALM_localhost.test(url); //regExCALM_desarrollo.test(url) ||
 	if(enabled){ //URL of a CALM course!
 		//Consider this *valid* url: https://test.calm.dhamma.org/en/courses/119/course_applications/8/edit#day-0 
 		var app = "applications"; //lets trim the "/8/edit#day-0" (added by CAL when user navigates through CALM inner tabs) 
 		url_DDBS_course = url.substr(0,url.search(app) + app.length);;  //update the url
 		url_download_json = url_DDBS_course + ".json?place_reserved=true"; //Download students JSON from CALM
-		url_POST_seat_map = url_DDBS_course + "/assign_generated_hall_positions"; //Upload seating map to CALM
+		url_POST_seat_map = url_DDBS_course + "/assign_generated_hall_positions"; //Upload sitting map to CALM
 		url_course        = url_DDBS_course + "/"; //sp_tab will have to add "147518/edit" where 147518 is std.id 
 		console.log("background.js: %cVALID URL:  " + url_DDBS_course, "color: green; ");
 		calm_tabId = tab.id;
